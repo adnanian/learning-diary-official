5991afc15bdfbf1e95a42702dd0694fc417c3be0
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-12 21:01:10 -0600

    Create UserContext and Provider

---
+++ src/user.js
@@ -0,0 +1,9 @@
+// Create context
+const UserContext = React.createContext();
+
+// Create user provider
+function UserProvider( {children} ) {
+    return <UserContext.Provider value={null}>{children}</UserContext.Provider>
+}
+
+export { UserContext, UserProvider };
